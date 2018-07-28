3deb87ae30dc8cd22f11fd9ab056889c27ad2f41
AIREL <regis.leruste@free.fr>
2018-07-28 12:42:09 +0200

    Mie à jour du 28 juillet 2018

--- Arduino/e-p-433-v2/e-p-433-v2.ino
+++ Arduino/e-p-433-v2/e-p-433-v2.ino
@@ -23,6 +23,11 @@
  *- Michael Margolis, accesibles par les liens :
  *- https://www.pjrc.com/teensy/td_libs_Time.html
  *- https://github.com/PaulStoffregen/Time
+ *Il utile le code source dévelloppé par :
+ *- Fabien Batteix (Skywood) qui est le rédacteur en chef et administrateur du site Carnet du Maker, ainsi que le gérant de l'entreprise TamiaLab qui édite le site.
+ *- selon l'article https://www.carnetdumaker.net/articles/mesurer-une-temperature-avec-un-capteur-1-wire-ds18b20-et-une-carte-arduino-genuino/
+ *- en tenant compte que pour des raisons de sécurité inhérant au projet SCAO qu'il est indispensable que la capture de données en provenance
+ * des 2 thermomètres digitaux DB18B20 se réalise toujours dans le même ordre, en particulier après un changement de l'un ou des deux thermomètres.  
  */
 /**
  * 2 - Initialisation des paramètres
@@ -32,11 +37,11 @@
  
 /* Dépendance pour le bus 1-Wire */
 #include <OneWire.h> //Chargement de la librairie OneWire.h
-/* Broche du bus 1-Wire */
-const byte BROCHE_ONEWIRE = 17;
-const byte nb_cap = 0x02;
+/* Broches des 2 bus 1-Wire */
+const byte BROCHE_ONEWIRE_1 = 16;
+const byte BROCHE_ONEWIRE_2 = 17;
 
-/* Code de retour de la fonction getTemperature() */
+/* Code de retour des 2 fonctions getT1() et getT2() */
 enum DS18B20_RCODES {
   READ_OK,  // Lecture ok
   NO_SENSOR_FOUND,  // Pas de capteur
@@ -45,8 +50,9 @@ enum DS18B20_RCODES {
 };
 
 
-/* Création de l'objet OneWire pour manipuler le bus 1-Wire */
-OneWire ds(BROCHE_ONEWIRE);
+/* Création des 2 objets OneWire pour manipuler les 2 bus 1-Wire */
+OneWire ds1(BROCHE_ONEWIRE_1);
+OneWire ds2(BROCHE_ONEWIRE_2);
 /**
  * 2b - Mesures des tensions
  */
@@ -93,33 +99,27 @@ byte count = 1;//Initialisation du numéro du message
 #include <avr/sleep.h>
 /**
  * 3 - Fonctions spécifiques
- * 3a - Fonction d'acquisition de la température via un capteur DS18B20.
+ * 3a-1 - Fonction d'acquisition de la température via le 1er thermomètre digital DS18B20 (ds1).
+/**
+ * Fonction de lecture de la température via un capteur DS18B20 câblé sur le 1er bus ds1.
  */
-byte getTemperature(float *temperature, byte reset_search) {
-  byte data[9], addr[8]; 
-  // Si le booléen reset_search est vrai, on recommence la recherche de capteur de zéro.
-  //Ce booléen doit être à true lors de la lecture du premier capteur.. 
+byte getT1(float *T1, byte reset_search) {
+  byte data[9], addr[8];
   // data[] : Données lues depuis le scratchpad
   // addr[] : Adresse du module 1-Wire détecté
-  byte reg2 = {nb_cap};//Données à écrire dans le byte 2
-  byte reg3 = {0xFF};//Données à écrire dans le byte 3
-  byte reg4 = {0x7F};//Données à écrire dans le byte 4
-  /* Reset le bus 1-Wire si nécessaire (requis pour la lecture du premier capteur) */
+  
+  /* Reset le bus 1-Wire ci nécessaire (requis pour la lecture du premier capteur) */
   if (reset_search) {
-    ds.reset_search();
+    ds1.reset_search();
   }
  
   /* Recherche le prochain capteur 1-Wire disponible */
-  /*Quand la fonction ds.search(addr) est exécutée 
-  l'adresse du capteur se trouve dans l'array de 8 bytes addr 
-  Boolean operator ! (logical not)*/
-  if (!ds.search(addr)) {
+  if (!ds1.search(addr)) {
     // Pas de capteur
     return NO_SENSOR_FOUND;
   }
   
   /* Vérifie que l'adresse a été correctement reçue */
-  /* Compute a CRC check on an array of data. */
   if (OneWire::crc8(addr, 7) != addr[7]) {
     // Adresse invalide
     return INVALID_ADDRESS;
@@ -130,54 +130,91 @@ byte getTemperature(float *temperature, byte reset_search) {
     // Mauvais type de capteur
     return INVALID_SENSOR;
   }
-   
-   /* Write the bytes 2, 3 et 4 to scratchpad */
-   ds.reset();
-   ds.select(addr);
-   ds.write(0x4E);//Write scratchpad
-   ds.write(reg2);
-   ds.write(reg3);
-   ds.write(reg4);
-   delay(200);
-  //Copy scratchpad to EEPROM
-   ds.reset();
-   ds.select(addr);
-   ds.write(0x48);//Copy scratchpad to EEPROM
-   delay(200);
+ 
   /* Reset le bus 1-Wire et sélectionne le capteur */
-  ds.reset(); //Reset the 1-wire bus. Usually this is needed before communicating with any device. 
-  /* Select a device based on its address. */ 
-  ds.select(addr);
-   /* Lance une prise de mesure de température et attend la fin de la mesure */
-  ds.write(0x44, 1); //Write a byte, and leave power applied to the 1 wire bus. 
+  ds1.reset();
+  ds1.select(addr);
+  
+  /* Lance une prise de mesure de température et attend la fin de la mesure */
+  ds1.write(0x44, 1);
   delay(800);
   
   /* Reset le bus 1-Wire, sélectionne le capteur et envoie une demande de lecture du scratchpad */
-  ds.reset();
-  ds.select(addr);
-  ds.write(0xBE);//Read scratchpad
+  ds1.reset();
+  ds1.select(addr);
+  ds1.write(0xBE);
  
  /* Lecture du scratchpad */
   for (byte i = 0; i < 9; i++) {
-    data[i] = ds.read();
+    data[i] = ds1.read();
   }
-    
+   
   /* Calcul de la température en degré Celsius */
-  *temperature = (int16_t) ((data[1] << 8) | data[0]) * 0.0625; 
-  //Affichage de l'adresse du capteur
-  Serial.print(data[2]);
-  Serial.print(" - ");
-  Serial.print(" ad-cap : ");
-  for(byte i = 0; i < 8; ++i) {
-    if (addr[i] < 0x10) Serial.write('0');
-    Serial.print(addr[i], HEX);
-    Serial.write(' ');
+  *T1 = (int16_t) ((data[1] << 8) | data[0]) * 0.0625; 
+  
+  // Pas d'erreur
+  return READ_OK;
+}
+ /** 3a-2 - Fonction d'acquisition de la température via le 2ème thermomètre digital DS18B20 (ds2).
+ */
+byte getT2(float *T2, byte reset_search) {
+  byte data[9], addr[8];
+  // data[] : Données lues depuis le scratchpad
+  // addr[] : Adresse du module 1-Wire détecté
+  
+  /* Reset le bus 1-Wire ci nécessaire (requis pour la lecture du premier capteur) */
+  if (reset_search) {
+    ds2.reset_search();
   }
+ 
+  /* Recherche le prochain capteur 1-Wire disponible */
+  if (!ds2.search(addr)) {
+    // Pas de capteur
+    return NO_SENSOR_FOUND;
+  }
+  
+  /* Vérifie que l'adresse a été correctement reçue */
+  if (OneWire::crc8(addr, 7) != addr[7]) {
+    // Adresse invalide
+    return INVALID_ADDRESS;
+  }
+ 
+  /* Vérifie qu'il s'agit bien d'un DS18B20 */
+  if (addr[0] != 0x28) {
+    // Mauvais type de capteur
+    return INVALID_SENSOR;
+  }
+ 
+  /* Reset le bus 1-Wire et sélectionne le capteur */
+  ds2.reset();
+  ds2.select(addr);
+  
+  /* Lance une prise de mesure de température et attend la fin de la mesure */
+  ds2.write(0x44, 1);
+  delay(800);
+  
+  /* Reset le bus 1-Wire, sélectionne le capteur et envoie une demande de lecture du scratchpad */
+  ds2.reset();
+  ds2.select(addr);
+  ds2.write(0xBE);
+ 
+ /* Lecture du scratchpad */
+  for (byte i = 0; i < 9; i++) {
+    data[i] = ds2.read();
+  }
+   
+  /* Calcul de la température en degré Celsius */
+  *T2 = (int16_t) ((data[1] << 8) | data[0]) * 0.0625; 
+  
   // Pas d'erreur
   return READ_OK;
 }
 /**
  * 3b - Fonction mesure des tensions et calibration
+ * 3c - BITE
+ * 3d - Tranmission
+ * 3e - Horodatage
+ * 3f - Mode sleep
  */
 
 /** 4 - Fonction setup() **/
@@ -186,42 +223,50 @@ void setup() {
   //Serial.begin(9600);
   delay(1000);
   Serial.println("Bonjour SCAO");
-  //a) Acquisition des temtératures
+  //a) Acquisition des températures
   //b) Mesure des tensions
   analogReadResolution(13);
   //c) BITE
-  pinMode(led_pin_v, OUTPUT);
-  pinMode(led_pin_j, OUTPUT);
-  pinMode(led_pin_r, OUTPUT);
+  
   //d) Transmission
     // Initialise the IO and ISR
   vw_set_tx_pin(transmit_pin);
   vw_setup(2000);   // Bits per sec
   //e) Horodatage
-  setTime(18, 02, 00, 23, 05, 2018);
+  setTime(12, 05, 00, 28, 07, 2018);
   //f) Mode sleep
+  //g) Visualisation des résultats
+  pinMode(led_pin_v, OUTPUT);
+  pinMode(led_pin_j, OUTPUT);
+  pinMode(led_pin_r, OUTPUT);
 }
  
 /** 5 - Fonction loop() **/
 void loop() {
   Serial.begin(9600);
   //a) Acquisition des températures
-  float temperature[2];//Initialise une array pour mémoriser T1 et T2
-  /* Lit la température T1 */
-  if (getTemperature(&temperature[0], true) != READ_OK) {
+  float T1;
+  /* Lit la température ambiante à ~1Hz */
+  if (getT1(&T1, true) != READ_OK) {
     Serial.println(F("Erreur de lecture du capteur"));
     return;
   }
-  if (getTemperature(&temperature[1], false) != READ_OK) {
-    Serial.println(F("Erreur de lecture du capteur 2"));
+  /* Affiche la température */
+  float T2;
+  /* Lit la température ambiante à ~1Hz */
+  if (getT2(&T2, true) != READ_OK) {
+    Serial.println(F("Erreur de lecture du capteur"));
     return;
   }
+  /* Affiche la température */
+ 
+
 
   //b) Mesure des tensions
 
   //c) BITE
   int tmax=25;
-  if (temperature[0] >= tmax){digitalWrite(led_pin_r, HIGH);} else {digitalWrite(led_pin_r, LOW);}
+  if (T2 >= tmax){digitalWrite(led_pin_r, HIGH);} else {digitalWrite(led_pin_r, LOW);}
   if (Vbat <= Vbat_limite && Vbat > Vbat_min){digitalWrite(led_pin_v, HIGH);} else {digitalWrite(led_pin_v, LOW);}
   if (Vbat > Vbat_limite || Vbat <= Vbat_cut_off){digitalWrite(led_pin_r, HIGH);} else {digitalWrite(led_pin_r, LOW);}
 
@@ -259,11 +304,15 @@ void loop() {
   Serial.print(" - ");
   /* Affiche T1 et T2 */
   Serial.print(F("T1 : "));
-  Serial.print(temperature[0], 1);
-  Serial.print(F("   -  T2 : "));
-  Serial.print(temperature[1], 1);
-  Serial.print(" degrés ");
-  Serial.write('C'); // Caractère degré
+  Serial.print(T1, 2);
+  Serial.write(" degrés "); // Caractère degré
+  Serial.write('C');
+  Serial.print(" - ");
+  Serial.print(F("T2 : "));
+  Serial.print(T2, 2);
+  Serial.write(" degrés "); // Caractère degré
+  Serial.write('C');
+  Serial.print(" ");
   /* Affiche les tensions */
   Vusb=map (2.0038*analogRead(Vusb_demie), 0, MaxConv, 0, MaxVolt);
   Serial.print(" - Vusb : ");
--- Arduino/test/test.ino
+++ Arduino/test/test.ino
@@ -1,40 +1,180 @@
-//test de la batterie Lipo
-#define mesure_1 A7 //Mesure de la tension Vbat
-#define mesure_2 A8 //Mesure de la tension Vusb
-#define mesure_3 A9 //Mesure de la tension 3.3V
-#define MaxConv 8192
-#define MaxVolt 3272
-//Fontion mesure de tension
-long int mesure(int i){
-long int m;
-m=map (2.0038*analogRead(mesure_1), 0, MaxConv, 0, MaxVolt);
-long int rm[4];
-rm[1]=m;
-m=map (2.0038*analogRead(mesure_2), 0, MaxConv, 0, MaxVolt);
-rm[2]=m;
-m=map (2.0038*analogRead(mesure_3), 0, MaxConv, 0, MaxVolt);
-rm[3]=m;
-return rm[i];
+/**
+ * Exemple de code pour lire un unique capteur DS18B20 sur un bus 1-Wire.
+ */
+ 
+/* Dépendance pour le bus 1-Wire */
+#include <OneWire.h>
+/* Broche du bus 1-Wire */
+const byte BROCHE_ONEWIRE_1 = 16;
+const byte BROCHE_ONEWIRE_2 = 17;
+/* Code de retour de la fonction getT1() */
+enum DS18B20_RCODES {
+  READ_OK,  // Lecture ok
+  NO_SENSOR_FOUND,  // Pas de capteur
+  INVALID_ADDRESS,  // Adresse reçue invalide
+  INVALID_SENSOR  // Capteur invalide (pas un DS18B20)
+};
+
+
+/* Création de l'objet OneWire pour manipuler le bus 1-Wire */
+OneWire ds1(BROCHE_ONEWIRE_1);
+OneWire ds2(BROCHE_ONEWIRE_2);
+const int led_pin_v = 13;//Led verte
+const int led_pin_j = 14;//Led jaune
+const int led_pin_r = 15;//Led rouge
+ 
+/**
+ * Fonction de lecture de la température via un capteur DS18B20 câblé sur le 1er bus ds1.
+ */
+byte getT1(float *T1, byte reset_search) {
+  byte data[9], addr[8];
+  // data[] : Données lues depuis le scratchpad
+  // addr[] : Adresse du module 1-Wire détecté
+  
+  /* Reset le bus 1-Wire ci nécessaire (requis pour la lecture du premier capteur) */
+  if (reset_search) {
+    ds1.reset_search();
+  }
+ 
+  /* Recherche le prochain capteur 1-Wire disponible */
+  if (!ds1.search(addr)) {
+    // Pas de capteur
+    return NO_SENSOR_FOUND;
+  }
+  
+  /* Vérifie que l'adresse a été correctement reçue */
+  if (OneWire::crc8(addr, 7) != addr[7]) {
+    // Adresse invalide
+    return INVALID_ADDRESS;
+  }
+ 
+  /* Vérifie qu'il s'agit bien d'un DS18B20 */
+  if (addr[0] != 0x28) {
+    // Mauvais type de capteur
+    return INVALID_SENSOR;
+  }
+ 
+  /* Reset le bus 1-Wire et sélectionne le capteur */
+  ds1.reset();
+  ds1.select(addr);
+  
+  /* Lance une prise de mesure de température et attend la fin de la mesure */
+  ds1.write(0x44, 1);
+  delay(800);
+  
+  /* Reset le bus 1-Wire, sélectionne le capteur et envoie une demande de lecture du scratchpad */
+  ds1.reset();
+  ds1.select(addr);
+  ds1.write(0xBE);
+ 
+ /* Lecture du scratchpad */
+  for (byte i = 0; i < 9; i++) {
+    data[i] = ds1.read();
+  }
+   
+  /* Calcul de la température en degré Celsius */
+  *T1 = (int16_t) ((data[1] << 8) | data[0]) * 0.0625; 
+  
+  // Pas d'erreur
+  return READ_OK;
 }
-const int led_pin = 13;
+/**
+ * Fonction de lecture de la température via un capteur DS18B20 câblé sur le 2ème bus ds2.
+ */
+byte getT2(float *T2, byte reset_search) {
+  byte data[9], addr[8];
+  // data[] : Données lues depuis le scratchpad
+  // addr[] : Adresse du module 1-Wire détecté
+  
+  /* Reset le bus 1-Wire ci nécessaire (requis pour la lecture du premier capteur) */
+  if (reset_search) {
+    ds2.reset_search();
+  }
+ 
+  /* Recherche le prochain capteur 1-Wire disponible */
+  if (!ds2.search(addr)) {
+    // Pas de capteur
+    return NO_SENSOR_FOUND;
+  }
+  
+  /* Vérifie que l'adresse a été correctement reçue */
+  if (OneWire::crc8(addr, 7) != addr[7]) {
+    // Adresse invalide
+    return INVALID_ADDRESS;
+  }
+ 
+  /* Vérifie qu'il s'agit bien d'un DS18B20 */
+  if (addr[0] != 0x28) {
+    // Mauvais type de capteur
+    return INVALID_SENSOR;
+  }
+ 
+  /* Reset le bus 1-Wire et sélectionne le capteur */
+  ds2.reset();
+  ds2.select(addr);
+  
+  /* Lance une prise de mesure de température et attend la fin de la mesure */
+  ds2.write(0x44, 1);
+  delay(800);
+  
+  /* Reset le bus 1-Wire, sélectionne le capteur et envoie une demande de lecture du scratchpad */
+  ds2.reset();
+  ds2.select(addr);
+  ds2.write(0xBE);
+ 
+ /* Lecture du scratchpad */
+  for (byte i = 0; i < 9; i++) {
+    data[i] = ds2.read();
+  }
+   
+  /* Calcul de la température en degré Celsius */
+  *T2 = (int16_t) ((data[1] << 8) | data[0]) * 0.0625; 
+  
+  // Pas d'erreur
+  return READ_OK;
+}
+ 
+/** Fonction setup() **/
 void setup() {
-  pinMode(led_pin, OUTPUT);
-  analogReadResolution(13);
-  Serial.begin(9600);
+
+  /* Initialisation du port série */
+  Serial.begin(115200);
+  pinMode(led_pin_v, OUTPUT);
+  pinMode(led_pin_j, OUTPUT);
+  pinMode(led_pin_r, OUTPUT);
+}
+ 
+ 
+/** Fonction loop() **/
+void loop() {
+  digitalWrite(led_pin_r, HIGH);
+  float T1;
+  /* Lit la température ambiante à ~1Hz */
+  if (getT1(&T1, true) != READ_OK) {
+    Serial.println(F("Erreur de lecture du capteur"));
+    return;
+  }
+  /* Affiche la température */
+  Serial.print(F("T1 : "));
+  Serial.print(T1, 2);
+  Serial.write(" degrés "); // Caractère degré
+  Serial.write('C');
+  Serial.print(" - ");
+  float T2;
+  /* Lit la température ambiante à ~1Hz */
+  if (getT2(&T2, true) != READ_OK) {
+    Serial.println(F("Erreur de lecture du capteur"));
+    return;
+  }
+  /* Affiche la température */
+  Serial.print(F("T2 : "));
+  Serial.print(T2, 2);
+  Serial.write(" degrés "); // Caractère degré
+  Serial.write('C');
+  Serial.println();
+  digitalWrite(led_pin_r, LOW);
+  delay(800);
 }
 
-void loop() {
-  digitalWrite(led_pin, HIGH);
-  delay(1000);
-  Serial.print(" Vusb : ");
-  Serial.print(mesure(2));
-  Serial.print(" mV ");
-  Serial.print(" Vbat : ");
-  Serial.print(mesure(1));
-  Serial.print(" mV ");
-  Serial.print(" V3.3 : ");
-  Serial.print(mesure(3));
-  Serial.println(" mV ");
-  digitalWrite(led_pin, LOW);
-  delay(1000);
-}
+
+
