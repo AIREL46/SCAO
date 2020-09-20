f9022a52231c7a390eee7148c4e0dfa0bcb7cf96
AIREL46 <regis.leruste@free.fr>
2020-09-20 18:45:00 +0200

    Mise à jour

--- Arduino/e-p-wifi/j_visu_ech.h
+++ Arduino/e-p-wifi/j_visu_ech.h
@@ -9,19 +9,22 @@
 * 3j - Visualisation du contenu des échantillons
 */
  void visu_demo(){
- Serial.print("Phase                      : ");
- Serial.println(phase);
+ //Serial.print("Phase                      : ");
+ //Serial.println(phase);
  Serial.print("t                          : ");
  Serial.println(t);
- Serial.print("T10pc                      : ");
- Serial.print(T10pc);//
- Serial.println(" degres Celsius");
+ //Serial.print("T10pc                      : ");
+ //Serial.print(T10pc);//
+ //Serial.println(" degres Celsius");
  Serial.print("temperature mesuree        : ");
  Serial.print(T2, 2);//Température mesurée T1 en °C sur la batterie
  Serial.println(" degres Celsius");
- Serial.print("T98pc                      : ");
- Serial.print(T98pc);//Température mesurée T1 en °C sur la batterie
+ Serial.print("temperature de consigne    : ");
+ Serial.print(Tcons, 2);//Température mesurée T1 en °C sur la batterie
  Serial.println(" degres Celsius");
+ //Serial.print("T98pc                      : ");
+ //Serial.print(T98pc);//Température mesurée T1 en °C sur la batterie
+ //Serial.println(" degres Celsius");
  Serial.print("duree iterative de chauffe : ");
  Serial.print(Dich, 2);//Température mesurée Tcons en °C de consigne (trajectoire)
  Serial.println(" secondes");
@@ -31,14 +34,14 @@
  Serial.print("cout                       : ");
  Serial.print(cout_total, 4);//Coût en €
  Serial.println(" euros");
- Serial.print("Puissance                  : ");
- Serial.print(puissance, 2);//Puissance
- Serial.println(" kilowatts");
+ //Serial.print("Puissance                  : ");
+ //Serial.print(puissance, 2);//Puissance
+ //Serial.println(" kilowatts");
  Serial.print("Somme des Dich             : ");
  Serial.print(som_dich, 2);//Somme des Dich
  Serial.println(" secondes");
- Serial.print("inter98pc                  : ");
- Serial.println(inter98pc);
+ //Serial.print("inter98pc                  : ");
+ //Serial.println(inter98pc);
  Serial.print("FC1                        : ");
  Serial.println(FC1);
  Serial.println();
