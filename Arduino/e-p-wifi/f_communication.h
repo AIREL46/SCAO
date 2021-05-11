1d7204b8321b85046586a9ee29698d896d7f02ce
AIREL46 <regis.leruste@free.fr>
2021-05-11 18:23:25 +0200

    mise à jour

---
+++ Arduino/e-p-wifi/f_communication.h
@@ -0,0 +1,16 @@
+/*
+* Communication e-r-wifi
+* 1 - Introduction
+*/
+/*
+* 2 - Initialisation des paramètres
+*/
+/*
+* 3 - Fonctions spécifiques
+*/
+/*
+* 4 - Fonction setup
+*/
+/*
+* 5 - Fonction loop
+*/
